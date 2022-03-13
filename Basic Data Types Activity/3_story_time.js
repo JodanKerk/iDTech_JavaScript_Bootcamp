5121b87122a3d2447d53ec09f580e46918885c30
JodanKerk <jodan9463@gmail.com>
2022-03-13 11:36:54 +0800

    Adding PacMan, Debugging and Story Time

---
+++ Basic Data Types Activity/3_story_time.js
@@ -0,0 +1,37 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+let heroName = "Captain Underpants";
+let villainName = "Butt Monster";
+let worldName = "Gottem' City";
+let villainGreeting = "Hohohoy!";
+
+// TODO: Create 3-Integer variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+let heroAge = 10000;
+let villainAge = 5000;
+let currentYear = 10000;
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+let weapons = ["Shiny Underpants", "Monster Trap", "Perfume Hydrant", "Ugly Butt", "Stinky Fart"];
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+let safe = true;
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+console.log (villainGreeting + " Welcome to " + worldName + "!");
+console.log ("I am " + villainName + ", how fortunate to have me today! I have been here for " + villainAge + "-");
+console.log ();
+console.log ("Shut it! I am " + heroName + ", I have been here for twice as long, I am now " + heroAge + "!");
+console.log ("Thats why the current year is " + currentYear + ", " + worldName + " is as old as I am!");
+console.log ();
+console.log (villainName +": I am going to flash my " + weapons[3] + " and finish you once and for all!");
+console.log (heroName + ": Do not worry everyone, I will use my " + weapons[0] + " to block your eyes!");
+console.log (villainName + ": I will use my " + weapons[4] + " to block the light from your " + weapons[0] + " so everyone can see my beautiful butt!");
+console.log (heroName + ": It's ok, I will use my " + weapons[2] + " so you don't need to smell the stink!");
+console.log (heroName + ": I'm going to end this once and for all and banish him to the depths of my " + weapons[1] + "!");
+console.log (villainName + ": Nooooooooo!");
+console.log ();
+console.log ("After 10000 years, " + heroName + "'s dreams have come " + safe + " and " + worldName + " is finally safe from " + villainName + ". Everyone can continue living their normal lives at last!")
